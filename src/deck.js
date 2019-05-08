4356c291b0c1ca15f1fe72847a1d0f5deace9140
Hangfish <dan.lofgrens@gmail.com>
2019-05-08 17:07:03 +0200

    Handling simple gestures across entire deck

--- src/deck.js
+++ src/deck.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 export default class Deck extends Component {
-    renderCards() {
+    position = new Animated.ValueXY();
+
+    panResponder = new PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+            this.position.setValue({ x: gesture.dx, y: gesture.dy })
+        },
+        onPanResponderEnd: (event, gesture) => {
+        }
+    });
+
+    state = {
+        panResponder: this.panResponder,
+        position: this.position
+    }
+
+    renderCards = () => {
         return this.props.data.map(item => {
             return this.props.renderCard(item);
         });
-    }
+    };
+
     render() {
-        return (
-            <View>
-                {this.renderCards()}
-            </View>
-        );
+        return <Animated.View
+            style={this.state.position.getLayout()}
+            {...this.panResponder.panHandlers}>{this.renderCards()}
+        </Animated.View>
     }
 }
